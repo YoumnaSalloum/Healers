030b77a675c9405ca88ff160efe938ad8185d549
YoumnaSalloum <youmna.salloum98@gmail.com>
2020-07-20 10:48:43 +0300

    (feat)logout and schema

--- server-side/routes/users.js
+++ server-side/routes/users.js
@@ -1,83 +1,95 @@
-const express = require('express')
 const mongoose = require('mongoose')
-//
-var bodyParser = require('body-parser');
-// //for testing
-// app.get('/',function(req,res){
-//     res.send("youmna")
-// })
-const users = express.Router();
-const cors = require('cors');
-require('dotenv').config(); // to read .env file
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const User = require('../../db/mongo');
-
-users.use(cors());
-users.post('/signUp', (req, res) => {
-
-    const userData = {
-        userName : req.body.myData.userName,
-        phoneNumber: req.body.myData.phoneNumber,
-        email: req.body.myData.email,
-        password: req.body.myData.password
-    }
-    console.log(req.body.myData)
-        User.findOne({
-            email: req.body.myData.email
-        })
-        .then(user => {
-            if(!user) {
-                bcrypt.hash(req.body.myData.password, 10, (err, hash) => {
-                    userData.password = hash;
-                    User.create(userData)
-                    .then(user => {
-                        res.json({status: user.email + 'added'})
-                    })
-                    .catch(err=> {
-                        res.send('error: ' + err)
-                    })
-                })
-
-            } else {
-                        
-                 res.json({error: 'email already exist'})
-            }
-        })
-        .catch(res => {
-            res.send('error: ' + err)
-        })
-    // }
-    // else{console.log("error")}
-})
-users.post('/login', (req, res) => {
-  
-    User.findOne({
-        email: req.body.myData.email
-    })
-    .then(user => {
-        if(user) {
-            if(bcrypt.compareSync(req.body.myData.password, user.password)) {
-                const payload = {
-                    _id: user._id,
-                    password: user.password,
-                    email: user.email
-                }
-                let token = jwt.sign(payload, process.env.JWT_KEY+"", {
-                    expiresIn: 1440
-                })
-                console.log(payload)
-                res.send(token)
-            } else {
-                res.json({error: "User dose not exist"})
-            }
-        } else {
-            res.json({error: "User dose not exist"})
-        }
-        // console.log(process.env.JWT_KEY)
-    })
-    .catch(err => {
-        res.send('error: ' + err)
-    })
-})
-module.exports=users
+const Schema = mongoose.Schema
+mongoose.connect("mongodb://localhost/collection");
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("connection has been made");
+  });
+db.on("error", (error) => {
+    console.log("Connection error:", error);
+  });
+//youmna
+//const foodSchema=new Schema({typeOfDisease:String,descreptionOfPrescription:String,phoneNumber:Number,img:String,video:String})
+const hospitalBillSchema=new Schema({amount:Number,hospitalName:String})
+const User=new Schema({
+    userName : 
+    {
+        type: String,
+        required: true,
+    },
+phoneNumber:
+{ 
+      type:
+       Number,
+    required: true
+},
+ email : 
+ {
+    type:
+     String,
+    required: true,
+    unique: true
+},
+password : 
+{
+    type: 
+    String,
+    required: true,
+},
+hospitalBill:[hospitalBillSchema]
+}
+)
+ const userModel=mongoose.model('user',User)
+// const userSignUp = new Schema({
+//     userName:String,phoneNumber:Number,email:String,password:String
+// }
+// )
+// const user = mongoose.model('user',userSignUp)
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+mongoose.connect("mongodb://localhost/collection");
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("connection has been made");
+  });
+db.on("error", (error) => {
+    console.log("Connection error:", error);
+  });
+//youmna
+//const foodSchema=new Schema({typeOfDisease:String,descreptionOfPrescription:String,phoneNumber:Number,img:String,video:String})
+const hospitalBillSchema=new Schema({amount:Number,hospitalName:String})
+const User=new Schema({
+    userName : 
+    {
+        type: String,
+        required: true,
+    },
+phoneNumber:
+{ 
+      type:
+       Number,
+    required: true
+},
+ email : 
+ {
+    type:
+     String,
+    required: true,
+    unique: true
+},
+password : 
+{
+    type: 
+    String,
+    required: true,
+},
+hospitalBill:[hospitalBillSchema]
+}
+)
+ const userModel=mongoose.model('user',User)
+// const userSignUp = new Schema({
+//     userName:String,phoneNumber:Number,email:String,password:String
+// }
+// )
+// const user = mongoose.model('user',userSignUp)
+module.exports = userModel

bc6a5e0e5d6fa2c36fc80ac9f5659767184caba6
Lubna-Hajhussein <lubnahaj45@gmail.com>
2020-08-04 19:46:02 +0300

    done with media upload creating post and rendering the data

--- server-side/routes/Users.js
+++ server-side/routes/Users.js
@@ -7,7 +7,6 @@ var multer = require('multer')
 var nodemailer = require('nodemailer');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -15,9 +14,6 @@ var transporter = nodemailer.createTransport({
       pass: 'youmna1998'
     }
   });
-
-
-
 //connect the route with User.js schema
 const users = express.Router();
 const cors = require('cors');
@@ -25,17 +21,12 @@ require('dotenv').config(); // to read .env file
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 users.use(cors());
-
 //route for profile 
     users.post("/mypost",(req,res)=>{
         User.findOne({id:req.body.email}).then(function (result) {
-         
           console.log(result);
            res.json(result);})
       })
-
-
-
 users.post('/signUp', (req, res) => {
     const userData = {
         userName : req.body.myData.userName,
@@ -44,7 +35,6 @@ users.post('/signUp', (req, res) => {
         password: req.body.myData.password,
         id:req.body.myData.email
     }
-    
     console.log(req.body.myData)
         User.findOne({
             email: req.body.myData.email
@@ -68,7 +58,6 @@ users.post('/signUp', (req, res) => {
         .catch(res => {
             res.send('error: ' + err)
         })
-
 })
 var obj={}
 var obj2={email:''}
@@ -80,7 +69,6 @@ users.post('/login', (req, res) => {
         if(user) {
             if(bcrypt.compareSync(req.body.myData.password, user.password)) {
                 req.session.loggedin = true;
-                    
                 const payload = {
                     _id: user._id,
                     password: user.password,
@@ -88,11 +76,9 @@ users.post('/login', (req, res) => {
                 }
                 obj.id = user._id
                 obj2.email=user.email
-               
                 let token = jwt.sign(payload, process.env.JWT_KEY+"", {
                     expiresIn: 1440
                 })
-               
                 res.send(token)
             } else {
                 res.json({error: "User dose not exist"})
@@ -100,7 +86,6 @@ users.post('/login', (req, res) => {
         } else {
             res.json({error: "User dose not exist"})
         }
-        
     })
     .catch(err => {
         res.send('error: ' + err)
@@ -118,11 +103,8 @@ users.post('/send',(req,res)=>{
    var selected=req.body.selected
    var feed=req.body.feed
    var id=req.body.id
-  
         User.findOne({id:id}).then(function(result){
-     
         console.log("youmna "+result)
-       
                 var mailOptions = {
                     from: 'youmna61998@gmail.com',
                     to:obj2.email,
@@ -137,11 +119,9 @@ users.post('/send',(req,res)=>{
                       console.log('Email sent: ' + info.response);
                     }
                });
-            
         res.send('youmna send: ')
         })
    })
-
 const storage = multer.diskStorage({
     destination: "./public/uploads/",
     filename: function (req, file, cb) {
@@ -152,18 +132,15 @@ const storage = multer.diskStorage({
     storage: storage,
     limits: { fileSize: 11000000 },
   }).single("myImage");
- 
   users.post("/upload", function (req, res) {
   var imgurl="";
     console.log(req.body);
     upload(req, res, function (err) {
       var hosBill = JSON.parse(req.body.Billdata);
       console.log(hosBill.amount)
-     
       console.log(hosBill.id)
        imgurl+= req.file.destination+ req.file.filename
        console.log(imgurl)
-
       console.log("Request file ---", req.file.destination+ req.file.filename); //Here you get file.
     //push bill for hospitalbill array
     // amount:{ hospitalName:{ hospitalPhoneNumber:{ hospitalAddress:{ descAboutHealthPatient: patientPhoneNumber:{
@@ -178,9 +155,7 @@ const storage = multer.diskStorage({
     //     feedBack: $("#feed").val(),
     //   };
       // postedBy:{
-
       console.log("Request file ---", req.file.path); //Here you get file.
-
           User.findOneAndUpdate(
              {id:hosBill.id},
              { $push: { hospitalBill:{amount:hosBill.amount,hospitalName:hosBill.hospitalName,
@@ -199,34 +174,22 @@ const storage = multer.diskStorage({
         return res.send(200).end();
       }
     });
- 
   });
-
   users.post("/delete", function (req, res) {
       //userid
       console.log(req.body.myData)
     // var id=req.body.id
     var query = { patientPhoneNumber: req.body.myData.billId}
     var str=`hospitalBill.${req.body.myData.billId}`
-    
          User.findOne({id:req.body.myData.userid}).then(function(result){
-    
             console.log(result.hospitalBill.splice(req.body.myData.billId,1))
             result.save()
-
          })
     //     User.findOneAndDelete(query).then(function(result){
     //     // if(result.hospitalBill[0].amount===0){
-
     //     // }
-
     // })
-
-
-
-
   })
-
 //   users.post('/mypost',(req,res)=>{
 //     User.find({id:req.body.myData.id})
 //     .populate('postedBy',"_id userName")
@@ -264,12 +227,4 @@ users.post("/load", function (req, res) {
    }
  });
     })
-
-
-
-
-
-
-
-
 module.exports=users

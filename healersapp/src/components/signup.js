bab8bd4a289440c423a4f0bc7e6af4887158abae
Mayis-Mazin <you@example.com>
2020-08-05 13:35:44 +0300

    (feat):signUp validation

--- healersapp/src/components/signup.js
+++ healersapp/src/components/signup.js
@@ -12,7 +12,7 @@ import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import $ from 'jquery'
-
+var axios = require("axios");
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -36,16 +36,91 @@ const useStyles = makeStyles((theme) => ({
 class Signup extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      fields: {},
+      errors: {}
+  }
     this.classes = useStyles.bind(this);
     this.handleSignUp = this.handleSignUp.bind(this)
+   
   }
   handleSignUp(event){
-     var user = {userName:$('#firstName').val(),phoneNumber:$('#phoneNumber').val(),email:$('#email').val(),password:$('#password').val()}
-     $.post('http://localhost:8000/signUp',
-     { myData: user })
-     .done(function () { alert(user.userName); })
-     .fail(function (jqxhr, settings, ex) { alert('failed, ' + ex); });
+    if(this.handleValidation()){
+      var user = {userName:$('#firstName').val(),phoneNumber:$('#phoneNumber').val(),email:$('#email').val(),password:$('#password').val()}
+      $.post('http://localhost:8000/signUp',
+      { myData: user })
+      .done(function () { alert(user.userName); })
+      .fail(function (jqxhr, settings, ex) { alert('failed, ' + ex); });
+      alert("Form submitted");
+   }else{
+      alert("Form has errors.")
+   }
+ 
+    //  var user = {userName:$('#firstName').val(),phoneNumber:$('#phoneNumber').val(),email:$('#email').val(),password:$('#password').val()}
+    //  $.post('http://localhost:8000/signUp',
+    //  { myData: user })
+    //  .done(function () { alert(user.userName); })
+    //  .fail(function (jqxhr, settings, ex) { alert('failed, ' + ex); });
      }
+//validation 
+handleValidation(){
+  let fields = this.state.fields;
+  let errors = {};
+  let formIsValid = true;
+//phone number 
+if (typeof fields["phone"] !== "undefined") {
+         
+  var pattern = new RegExp(/^[0-9\b]+$/);
+  if (!pattern.test(fields["phone"])) {
+    formIsValid = false;
+    errors["phone"] = "Please enter only number.";
+  }else if(fields["phone"].length != 10){
+    formIsValid = false;
+    errors["phone"] = "Please enter valid phone number.";
+  }
+}
+//
+  //Name
+  if(!fields["name"]){
+     formIsValid = false;
+     errors["name"] = "Cannot be empty";
+  }
+  if(typeof fields["name"] !== "undefined"){
+     if(!fields["name"].match(/^[a-zA-Z]+$/)){
+        formIsValid = false;
+        errors["name"] = "Only letters";
+     }        
+  }
+  //Email
+  if(!fields["email"]){
+     formIsValid = false;
+     errors["email"] = "Cannot be empty";
+  }
+  if(typeof fields["email"] !== "undefined"){
+     let lastAtPos = fields["email"].lastIndexOf('@');
+     let lastDotPos = fields["email"].lastIndexOf('.');
+     if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+        formIsValid = false;
+        errors["email"] = "Email is not valid";
+      }
+ }  
+ this.setState({errors: errors});
+ return formIsValid;
+}
+contactSubmit(e){
+  e.preventDefault();
+  if(this.handleValidation()){
+     alert("Form submitted");
+  }else{
+     alert("Form has errors.")
+  }
+}
+handleChange(field, e){         
+  let fields = this.state.fields;
+  fields[field] = e.target.value;        
+  this.setState({fields});
+}
+//
   render() {
     return (
       <div >
@@ -54,8 +129,7 @@ class Signup extends Component {
           <Typography variant="h6" >
             About Us
           </Typography>
-
-          <Button  color="inherit" to="/" component={Link} >Logout</Button>
+            
         </Toolbar>
       </AppBar>
       <Container component="main" maxWidth="xs">
@@ -68,10 +142,12 @@ class Signup extends Component {
         Sign up
       </Typography>
       <br />
-      <form className={this.classes.form} noValidate>
+      <form name="contactform" className="contactform" onSubmit= {this.contactSubmit.bind(this)}>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
             <TextField
+            ref="name" 
+            onChange={this.handleChange.bind(this, "name")} value={this.state.fields["name"]}
               autoComplete="fname"
               name="firstName"
               variant="outlined"
@@ -81,9 +157,14 @@ class Signup extends Component {
               label="userName"
               autoFocus
             />
+            
+            <span style={{color: "red"}}>{this.state.errors["name"]}</span>
+            <br/>
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
+            ref='phone'
+            onChange={this.handleChange.bind(this, "phone")} value={this.state.fields["phone"]}
               variant="outlined"
               required
               fullWidth
@@ -92,9 +173,13 @@ class Signup extends Component {
               name="lastName"
               autoComplete="lname"
             />
+            <span style={{color: "red"}}>{this.state.errors["phone"]}</span>
+            <br/>
           </Grid>
           <Grid item xs={12}>
             <TextField
+            ref="name" 
+            onChange={this.handleChange.bind(this, "email")} value={this.state.fields["email"]}
              type="email"
               variant="outlined"
               required
@@ -128,7 +213,7 @@ class Signup extends Component {
           color="primary"
           className={this.classes.submit}
           id='signUp'
-          component={Link} 
+          // component={Link} 
           to="/login"
           onClick={this.handleSignUp}
         >

f933c266cbc771c115aa29ff981d26aebcc1d525
YoumnaSalloum <youmna.salloum98@gmail.com>
2020-08-05 10:24:41 +0300

    (test) make test for db save and connection for db

---
+++ test/saving_test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const User = require('../db/mongo');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+// Describe our tests
+describe('Saving records', function () {
+
+    // Create tests
+    it('Saves a record to the database', function (done) {
+
+        const char = new User({
+            userName : "hamadeh",
+            phoneNumber:34567867890,
+            email:'hamadeh@gmail.com',
+            password : '1234',
+            id:'hamadeh@gmail.com'
+        });
+
+        char.save().then(function () {
+            assert(char.isNew===false);
+            done();
+        });
+
+    });
+
+});
